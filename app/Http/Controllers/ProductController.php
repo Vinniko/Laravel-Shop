c1e6c211319265f37602d03d2f959738334e318d
Vinniko <vinnik_21@bk.ru>
2021-06-21 10:04:38 +0300

    Update filter logic commit
    Будет обнволена логика фльтра товаров, путем добавления нового json поля options и использования оператора like для поиска подходящих значений в базе данных

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,33 +14,14 @@ class ProductController extends Controller
    }
 
    public function filter(Request $request){
-       $title = "";
-       $min_price = 0;
-       $max_price = 60000;
-       $min_qty = 0;
-       $max_qty = 60000;
-       if($request->has('min_price')) $min_price = $request->input('min_price');
-       if($request->has('max_price')) $max_price = $request->input('max_price');
-       if($request->has('min_qty'))  $min_qty =$request->input('min_qty');
-       if($request->has('max_qty')) $max_qty =$request->input('max_qty');
-       $products = Product::where('price', '>', $min_price)
-            ->where('price', '<', $max_price)
-            ->where('qty', '>', $min_qty)
-            ->where('qty', '<', $max_qty)
-            ->paginate(40);
-        $result = [];
-        if($request->has('title')){
-            $title = $request->input('title');
-            foreach ($products as $product)
-            {
-                if(mb_strpos($product->title, $title) !== false){
-                    array_push($result, $product);
-                }
-            }
-        } 
-        else{
-            $result = $products;
-        }
-        return response()->json($result);
+       $options = [];
+       if($request->has('filters')) $options = $request->input('filters');
+       $keys = array_keys($options);
+       $conditions = [];
+       foreach($keys as $key){
+            array_push($conditions, ['options', 'like', '%"'.$key.'": "'.$options[$key].'"%']);
+       }
+       $products = Product::where($conditions)->paginate(40);
+       return response()->json($products);
    }
 }
