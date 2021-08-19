74e3d9c83c08f0445ddfb4c310904cc42705d0d5
Aleksey Vinnik <vinniko333@gmail.com>
2021-08-19 14:21:49 +0300

    Register search classes in AppServiceProvider commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ISearcher;
+use App\Services\ElasticsearchSearcher;
+use App\Services\ProductsSearcher;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +19,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ISearcher::class, function () {
+            if (! config('services.search.enabled')) {
+                return new ProductsSearcher();
+            }
+            return new ElasticsearchSearcher(
+                $this->app->make(Client::class)
+            );
+        });
+        $this->bindSearchClient();
     }
 
     /**
@@ -25,5 +38,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 }
