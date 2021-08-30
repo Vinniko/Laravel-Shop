416cd99ec43fa59e21714b7c1ca84c8785835390
Aleksey Vinnik <vinniko333@gmail.com>
2021-08-30 09:26:09 +0300

    Create route for testing RabbitMQ commit

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'products'], function (){
-    Route::get('/all', [App\Http\Controllers\ProductController::class, 'index'])->middleware(['auth:api']);
+    Route::get('/all', [App\Http\Controllers\ProductController::class, 'index']);#->middleware(['auth:api']);
     Route::get('/filter', [App\Http\Controllers\ProductController::class, 'filter'])->middleware(['auth:api']);
     Route::get('/search/{query}', [App\Http\Controllers\ProductController::class, 'search'])->middleware(['auth:api']);
     Route::get('/get/{product:id}', [App\Http\Controllers\ProductController::class, 'show'])->middleware(['auth:api']);
     Route::post('/store', [App\Http\Controllers\ProductController::class, 'store'])->middleware(['auth:api']);
     Route::put('/edit/{product:id}', [App\Http\Controllers\ProductController::class, 'edit'])->middleware(['auth:api']);
-    Route::delete('/delete/{product:id}', [App\Http\Controllers\ProductController::class, 'delete'])->middleware(['auth:api']);
+    Route::delete('/delete/{product:id}', [App\Http\Controllers\ProductController::class, 'delete']);#->middleware(['auth:api']);
+    Route::get('/reindex', [App\Http\Controllers\ProductController::class, 'reindex']);#->middleware(['auth:api']);
 });
